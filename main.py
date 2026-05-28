fa2d88ef40f69ce1872a4787ff6d7fc1ae90f521
aygulai-dev <aygul.aliyeva.dev@gmail.com>
2026-05-28 14:33:21 +0200

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Smart Home AI System - main file
+from data import get_sensor_data
+
+data = get_sensor_data()
+
+print("SMART HOME SYSTEM")
+print("------------------")
+
+print("Temperature:", data["temperature"])
+print("Humidity:", data["humidity"])
+print("Energy:", data["energy"])
+
+if data["temperature"] > 30:
+    print("WARNING: High temperature!")
+if data["energy"] > 500:
+    print("WARNING: High energy usage!")
