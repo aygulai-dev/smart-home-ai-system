8be17a51222b3590bd64f7af79669a9ac2541854
aygulai-dev <aygul.aliyeva.dev@gmail.com>
2026-05-28 14:33:47 +0200

    data.py

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
 # Simulated Smart Home Sensor Data
+import random
+
+def get_sensor_data():
+    return {
+        "temperature": random.randint(18, 40),
+        "humidity": random.randint(30, 80),
+        "energy": random.randint(100, 600)
+    }
